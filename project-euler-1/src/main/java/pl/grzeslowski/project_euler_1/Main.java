89c840ead3052bf31d1101dc3d09b443797cd0d5
Martin Grześlowski <martin.grzeslowski@gmail.com>
2013-12-03 11:05:20 +0100

    Implemented main

--- project-euler-1/src/main/java/pl/grzeslowski/project_euler_1/Main.java
+++ project-euler-1/src/main/java/pl/grzeslowski/project_euler_1/Main.java
@@ -2,9 +2,12 @@ package pl.grzeslowski.project_euler_1;
 
 public final class Main {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    public static void main(final String[] args) {
+        final int maxValue = Integer.parseInt(args[0]);
 
+        final ProjectEuler1 projectEuler1SimpleImpl = new ProjectEuler1SimpleImpl();
+
+        System.out.println(projectEuler1SimpleImpl.findSum(maxValue));
     }
 
 }
