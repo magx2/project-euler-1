46852bfdb8453e483812b08300f22026774cfbb8
Martin Grześlowski <martin.grzeslowski@gmail.com>
2013-12-03 10:38:27 +0100

    Adding mocks of classes

---
+++ project-euler-1/src/main/java/pl/grzeslowski/project_euler_1/ProjectEuler1.java
@@ -0,0 +1,5 @@
+package pl.grzeslowski.project_euler_1;
+
+public interface ProjectEuler1 {
+    long findSum(final int maxValue);
+}
---
+++ project-euler-1/src/main/java/pl/grzeslowski/project_euler_1/ProjectEuler1SimpleImpl.java
@@ -0,0 +1,10 @@
+package pl.grzeslowski.project_euler_1;
+
+public final class ProjectEuler1SimpleImpl implements ProjectEuler1 {
+
+    public long findSum(int maxValue) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
